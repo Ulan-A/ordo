0d60f1daaa9ddff8684c19870930504931c09a00
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2022-02-12 00:05:49 +0600

    allauth2

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-en'
 
 TIME_ZONE = 'UTC'
 
