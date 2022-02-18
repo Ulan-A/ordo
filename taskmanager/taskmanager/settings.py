2997ae41f43d87af1b6daf5ea4a90ed2d3769874
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2022-02-18 18:26:07 +0600

    allauth google2

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -74,7 +74,7 @@ AUTHENTICATION_BACKENDS = (
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
+
 )
 
 # Password validation
@@ -116,7 +116,7 @@ AUTHENTICATION_BACKENDS = [
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    ]
+]
 
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 1
 ACCOUNT_USERNAME_MIN_LENGTH = 4
@@ -141,4 +141,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 SITE_ID = 1
-
--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
+    path('account/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('', include('ordo.urls')),
 ]
