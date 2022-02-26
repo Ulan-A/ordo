cb54efb5c696cb4f6f268c9a3b13e1ab7163c879
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2022-02-26 17:18:40 +0600

    all fixed

--- taskmanager/ordo/models.py
+++ taskmanager/ordo/models.py
@@ -10,6 +10,6 @@ class Room(models.Model):
 
 class Message(models.Model):
     value = models.CharField(max_length=1000000)
-    date = models.DateTimeField(default=datetime.now, blank=True)
+    date = models.TimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)
